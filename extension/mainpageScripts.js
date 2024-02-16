a071f4bf2100736758b7ecea35c6f464e3e7b518
Josh Moskoff <joshmoskoff@carrick-12.dynamic.rpi.edu>
2024-02-16 17:37:16 -0500

    retrieves email list

--- extension/mainpageScripts.js
+++ extension/mainpageScripts.js
@@ -1,21 +1,25 @@
+import {getEmailList} from './emailDigest.js';
 // This function will be called when the Load Summaries button is clicked
 function loadSummaries() {
   console.log('summaries...');
 }
 
-// This function simulates fetching email summaries, replace with your actual data fetching logic
-// function fetchEmailSummaries() {}
+// This function simulates fetching email summaries, replace with your actual
+// data fetching logic function fetchEmailSummaries() {}
 
 // This function handles the display of email summaries on the page
 // function displayEmailSummaries(summaries) {}
 
-// Event listener for DOMContentLoaded to ensure the DOM is fully loaded before trying to access elements
+// Event listener for DOMContentLoaded to ensure the DOM is fully loaded before
+// trying to access elements
 document.addEventListener('DOMContentLoaded', (event) => {
   // Get the button by its ID and add an event listener
   var loadButton = document.getElementById('loadButton');
-  if(loadButton) {
-    loadButton.addEventListener('click', loadSummaries);
-  }else{
+  if (loadButton) {
+    loadButton.addEventListener('click', function() {
+      getEmailList();
+    });
+  } else {
     console.error('loadButton not found')
   }
 });
