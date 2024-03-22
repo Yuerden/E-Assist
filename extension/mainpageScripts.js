e8ca172e8b8cfb23ccff41e0c60bd281293bcba3
DalondB <dalondbidonne@gmail.com>
2024-03-22 16:27:08 -0400

    Revert "Test commit"
    This reverts commit d628fd7c06db1db7a6d0589261513e06657b4d74.

--- extension/mainpageScripts.js
+++ extension/mainpageScripts.js
@@ -12,40 +12,42 @@ function loadSummaries() {
 
 // Event listener for DOMContentLoaded to ensure the DOM is fully loaded before
 // trying to access elements
-// document.addEventListener('DOMContentLoaded', (event) => {
-//   var emailGetter = new EmailGetter();
-//   // Get the button by its ID and add an event listener
-//   var loadButton = document.getElementById('loadButton');
-//   if (loadButton) {
-//     loadButton.addEventListener('click', function() {
-//       chrome.storage.local.get('token', function(result) {
-//         emailGetter.getEmailList()
-//       });
-//     });
+document.addEventListener('DOMContentLoaded', (event) => {
+  var emailGetter = new EmailGetter();
+  // Get the button by its ID and add an event listener
+  var loadButton = document.getElementById('loadButton');
+  if (loadButton) {
+    loadButton.addEventListener('click', function() {
+      chrome.storage.local.get('token', function(result) {
+        emailGetter.getEmailList()
+      });
+    });
 
-//   } else {
-//     console.error('loadButton not found')
-//   }
-//   var nextEmailButton = document.getElementById('nextEmail');
-//   if (nextEmailButton) {
-//     nextEmailButton.addEventListener('click', function() {
-//       emailGetter.getNextEmail()
-//     });
-//   }
-// });
-
-// The above is just testing for now
-// Make a new button just for populating all email summaries
+  } else {
+    console.error('loadButton not found')
+  }
+  var nextEmailButton = document.getElementById('nextEmail');
+  if (nextEmailButton) {
+    nextEmailButton.addEventListener('click', function() {
+      emailGetter.getNextEmail()
+    });
+  }
+});
 
 /* Carousel JS (Find a way to integrate into load button above)*/
-const carousel = document.getElementById('carousel');
+const carousel = document.querySelector('.carousel');
+const prevButton = document.querySelector('.prev-button');
+const nextButton = document.querySelector('.next-button');
 let currentIndex = 0;
 
+/*Initial Email Summary Slide*/
+addSlide();
+
 /*Next Email Function*/
-function slideNext(emaildata) {
+function slideNext() {
 
   currentIndex++;
-  addSlide(emaildata);
+  addSlide();
   updateSlide();
 }
 
@@ -62,59 +64,19 @@ function updateSlide() {
 
   const offset = -currentIndex * 100.57; //This is weird as fuck gotta change later, left movement too big right move ment too small
   carousel.style.transform = `translateX(${offset}%)`;
+
 }
 
 // Function to add a new slide
-function addSlide(emaildata) {
-
+function addSlide() {
   const slide = document.createElement('div');
   slide.classList.add('slide');
-  slide.textContent = 'New Email Slide ' + (currentIndex + 1) + emaildata;
+  slide.textContent = 'New Email Slide ' + (currentIndex + 1);
   
   carousel.appendChild(slide);
 }
 
-// Get Email Summaries function
-document.addEventListener('DOMContentLoaded', (event) => {
+/* When Button is clicked run functions respectively*/
+nextButton.addEventListener('click', slideNext);
+prevButton.addEventListener('click', slidePrev);
 
-  //Email Getter object
-  var emailGetter = new EmailGetter();
-  
-  // Get the button by its ID and add an event listener
-  // Initial Email Summary getter button
-  var loadButton = document.getElementById('loadButton');
-  if (loadButton) {
-    loadButton.addEventListener('click', function() {
-      chrome.storage.local.get('token', function(result) {
-        emailGetter.getEmailList()
-      });
-    });
-  }
-
-  else {
-    console.error('loadButton not found')
-  }
-
-  /*Initial Email Summary Slide*/
-  addSlide(emailGetter.getNextEmail());
-
-  //Next Button/Email Code
-  var nextButton = document.getElementById('next-button');
-  if (nextButton) {
-    nextButton.addEventListener('click', function() {
-      emaildata = emailGetter.getNextEmail()
-
-      slideNext(emaildata)
-    });
-  }
-
-  //Previous Button/Email Code
-  var prevButton = document.getElementById('prev-button');
-  if (prevButton){
-    prevButton.addEventListener('click', function(){
-      
-    slidePrev()  
-
-    });
-  }
-});
